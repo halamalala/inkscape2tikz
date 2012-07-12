6ea8099657ddb527949eef1db5f28d8c9cbcd6f1
Kjell Magne Fauske <Kjell Magne Fauske@localhost>
2012-07-12 20:39:55 +0200

    Improved easy_install/pip support

---
+++ doc/index.rst
@@ -0,0 +1,21 @@
+.. svg2tikz documentation master file
+
+SVG2TikZ -- SVG to TikZ converter
+=================================
+
+Svg2tikz is an Inkscape extension for exporting SVG paths as TikZ/PGF paths.
+Svg2tikz can also be used as a command line tool or as a Python module.
+
+Contents
+--------
+
+.. toctree::
+   :maxdepth: 2
+   
+   install.rst
+   inkscapeguide.rst
+   cmdlineguide.rst
+   moduleguide.rst
+
+Background and motivation
+-------------------------
---
+++ tests/test_svg2tikz.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from svg2tikz.extensions.tikz_export import convert_file, convert_svg
+#from svg2tikz.extensions.tikz_export import GraphicsState
+from lxml import etree
+from cStringIO import StringIO
+
+
+basic_svg = r"""<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
+  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="12cm" height="4cm" viewBox="0 0 1200 400"
+     xmlns="http://www.w3.org/2000/svg" version="1.1">
+  <desc>Example rect01 - rectangle with sharp corners</desc>
+  <!-- Show outline of canvas using 'rect' element -->
+  <rect x="1" y="1" width="1198" height="398" id="rect1"
+        fill="none" stroke="blue" stroke-width="2"/>
+  <rect x="400" y="100" width="400" height="200" id="rect2"
+        fill="yellow" stroke="navy" stroke-width="10"  />
+</svg>
+"""
+
+basic2_svg = r"""<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
+  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="12cm" height="4cm" viewBox="0 0 1200 400"
+     xmlns="http://www.w3.org/2000/svg" version="1.1">
+  <desc>Example rect01 - rectangle with sharp corners</desc>
+  <!-- Show outline of canvas using 'rect' element -->
+  <rect x="1" y="1" width="1198" height="398" id="rect1"
+        fill="none" stroke="blue" stroke-width="2"/>
+  <rect x="400" y="100" width="400" height="200" id="rect2"
+        fill="yellow" stroke="navy" stroke-width="10"  />
+  <rect x="400" y="100" width="400" height="200" id="rect3"
+        fill="none" stroke="green" stroke-width="10"  />
+</svg>
+"""
+
+class InterfaceTest(unittest.TestCase):
+    
+    def test_basicsvg(self):
+        code = convert_svg(basic_svg)
+        assert 'rect' in code
+    
+    def test_basic_codeonly(self):
+        code = convert_svg(basic_svg, codeoutput="codeonly")
+        assert 'documentclass' not in code
+        assert r'\begin{tikzpicture}' not in code
+
+    def test_basic_figonly(self):
+        code = convert_svg(basic_svg, codeoutput="figonly")
+        assert 'documentclass' not in code
+        assert r'\begin{tikzpicture}' in code
+        
+    def test_no_ids(self):
+        code = convert_svg(basic2_svg, ids=[], verbose=True)
+        assert 'rect1' in code
+        assert 'rect2' in code
+        
+    def test_select_id_rect1(self):
+        code = convert_svg(basic2_svg, ids=['rect1'], verbose=True)
+        assert 'rect1' in code
+        assert 'rect2' not in code
+        
+    def test_select_id_rect1and3(self):
+        code = convert_svg(basic2_svg, ids=['rect1', 'rect3'], verbose=True)
+        assert 'rect1' in code
+        assert 'rect2' not in code
+        assert 'rect3' in code
+    
+
+
+paint_svg = r"""<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
+  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="12cm" height="4cm" viewBox="0 0 1200 400"
+     xmlns="http://www.w3.org/2000/svg" version="1.1"
+     fill="red">
+  <desc>Example rect01 - rectangle with sharp corners</desc>
+  <!-- Show outline of canvas using 'rect' element -->
+  <rect x="1" y="1" width="1198" height="398"
+     stroke="blue" stroke-width="2"/>
+</svg>
+"""
+
+class PaintingTest(unittest.TestCase):
+    
+    def test_inherited_fill(self):
+        code = convert_svg(paint_svg, codeoutput="codeonly")
+        assert 'fill=red' in code
+
+
+#class TestGraphicsState(unittest.TestCase):
+#    def test_basic(self):
+#        doc = etree.parse(StringIO(paint_svg))
+#        root = doc.getroot()
+#        state = GraphicsState(root)
+#        print state
+
+if __name__ == '__main__':
+    unittest.main()
